227cb8f89227e7cc047f68da6353eaa9e7539337
Kim K <koomen@protonmail.com>
2017-06-18 16:28:54 +0200

    fixed install of notification xfce4-notifyd

--- scripts/xfce4_notifications_enable.sh
+++ scripts/xfce4_notifications_enable.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+# i3 uses dunst on default so we want to remove that
+sudo apt-get purge dunst
+pkill dunst
+pkill notify-osd
+
+# install xfce-notifyd
 sudo apt-get install xfce4-notifyd
+
+# disable the default notification service
 sudo mv /usr/share/dbus-1/services/org.freedesktop.Notifications.service /usr/share/dbus-1/services/org.freedesktop.Notifications.service.disabled
